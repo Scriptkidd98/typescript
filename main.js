55d2a00bfec5917bf92b569b3883b517f76ad58d
Scriptkidd98 <duyilekan98@gmail.com>
2022-09-20 15:12:46 +0100

    add ts and js files

---
+++ main.js
@@ -0,0 +1,25 @@
+/* function log(msg) {
+    console.log(msg);
+}
+log("Hello World!");
+
+function doSomething() {
+    for(let i = 0; i < 5; i++) {
+        console.log(i);
+    }
+}
+
+doSomething(); */
+/* let count = 5;
+count = "a"; //cant reassign different variable type after initialization
+ */
+/* let a: any;
+a = 1;
+a = true;
+
+enum Color {Red, Green, Blue};
+let background = Color.Blue
+console.log(background); */
+var msg = "abc";
+var x = msg.endsWith("c");
+console.log(x);
