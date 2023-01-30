bbc8cba21a2a58bf64126f0de43412562680fa84
Isaac <duyilekan98@gmail.com>
2023-01-30 13:53:07 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -11,42 +11,6 @@ function doSomething() {
 }
 
 doSomething(); */
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 /* let count = 5;
 count = "a"; //cant reassign different variable type after initialization
  */
@@ -68,14 +32,12 @@ var person = {
 };
 myname = "Anon"; //initializing variables with their respective types
 age = 24;
-console.log(myname, age, person);
 var y = {
     a: "one",
     b: 1,
     c: "two",
     d: 2
 };
-console.log(y);
 var aname;
 var anage;
 var arandom;
@@ -84,7 +46,6 @@ var personas = {
 };
 aname = "John Doe";
 anage = 24;
-console.log(person);
 var Sophie = {
     name: 'Sophie',
     breed: 'Caucasian',
@@ -92,37 +53,26 @@ var Sophie = {
     age: 1,
     healthy: true
 };
-console.log(typeof (Sophie)); //Sophie variable is an object
-function f() {
-    return __awaiter(this, void 0, void 0, function () {
-        var aPromise, done, _a, _b;
-        return __generator(this, function (_c) {
-            switch (_c.label) {
-                case 0:
-                    aPromise = new Promise(function (resolve) {
-                        setTimeout(function () {
-                            resolve('Yo'); //after 3 seconds the promise is resolved
-                        }, 3000);
-                    });
-                    return [4 /*yield*/, aPromise];
-                case 1:
-                    done = _c.sent();
-                    console.log(done);
-                    _b = (_a = console).log;
-                    return [4 /*yield*/, aPromise];
-                case 2:
-                    _b.apply(_a, [_c.sent()]);
-                    return [2 /*return*/];
-            }
-        });
-    });
+//console.log(typeof(Sophie)); //Sophie variable is an object
+/* async function f() {
+  let aPromise = new Promise<string>(function(resolve){ //aPromise creates a promise, so like 'I promise you something, just wait for 3 seconds'
+    setTimeout(() => {
+      resolve('Yo') //after 3 seconds the promise is resolved
+    }, 3000);
+  });
+  let done = await aPromise; //done waits for what aPromise promised it. after 3 seconds aPromise gives done what it promised it
+  console.log(done);
+  console.log(await aPromise);
 }
-f();
+f() */
 var anArray = [2, 9, 7, 6, 7];
+anArray.map(function (data) {
+    //console.log(data)
+});
 var getFullName = function (firstName, lastName) {
     return firstName + " " + lastName;
 };
-console.log(getFullName('Isaac', 'Natty'));
+//console.log(getFullName('Isaac', 'Natty'))
 var user = {
     name: 'Isaac',
     age: 1
@@ -134,11 +84,27 @@ var user2 = {
         return user2.name;
     }
 };
-console.log(user2.getName());
 var user1 = {
-    name: 'Isaac',
-    age: 1,
-    getName: function () {
-        return user1.name;
+    dob: 1987,
+    setName: function (name) {
+        this.name = name;
+    },
+    calcAge: function (currentYear) {
+        this.age = currentYear - user1.dob;
+    },
+    setAccountType: function () {
+        this.age >= 18 ? this.accountType = 'Adult' : this.accountType = 'Minor';
     }
 };
+var createUser = function (name, currentYear) {
+    user1.setName(name);
+    user1.calcAge(currentYear);
+    user1.setAccountType();
+};
+createUser('David', 2023);
+console.log(user1.name);
+console.log(user1.age);
+console.log(user1.accountType);
+var dstArray = ['One', 'Two', 'Three', 'Four'];
+var one = dstArray[0], two = dstArray[1], three = dstArray[2], four = dstArray[3];
+//console.log(one);
